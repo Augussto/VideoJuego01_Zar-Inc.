ed87b3edbb5ac9e7a4c2f6262d4832dc44a7742a
Augussto <augusstocs@gmail.com>
2020-10-28 10:57:53 -0300

    Primer Commit - Movimientos Basicos

---
+++ Assets/Scripts/InputPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class InputPlayer : MonoBehaviour
+{
+
+    [HideInInspector]public float ejeHorizontal   {get; private set ;}
+    [HideInInspector]public float ejeVertical     {get; private set ;}
+                                                              
+    [HideInInspector] public bool atacar          {get; private set ;}
+    [HideInInspector] public bool habilidad1      {get; private set ;}
+    [HideInInspector]public bool habilidad2       {get; private set ;}
+    [HideInInspector]public bool inventario       {get; private set ;}
+    [HideInInspector] public bool interactuar     { get; private set;}
+
+    // Update is called once per frame
+    void Update()
+    {
+        atacar=Input.GetButtonDown("Atacar");
+        habilidad1 = Input.GetButtonDown("Habilidad1");
+        habilidad2 = Input.GetButtonDown("Habilidad2");
+        inventario = Input.GetButtonDown("Inventario");
+        interactuar = Input.GetButtonDown("Interactuar");
+       
+       
+        ejeHorizontal = Input.GetAxis("Horizontal");
+        ejeVertical = Input.GetAxis("Vertical");
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private InputPlayer inputJugador;
+    private Transform transformada;
+    private Rigidbody2D miRigidbody2D;
+    private Animator animator;
+    private SpriteRenderer miSprite;
+
+    int correrHashCode;
+   
+    [SerializeField]private float velocidad=3;
+    
+    private float horizontal, vertical;
+ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputJugador = GetComponent<InputPlayer>();
+        transformada = GetComponent<Transform>();
+        miRigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        miSprite = GetComponent<SpriteRenderer>();
+        correrHashCode = Animator.StringToHash("Corriendo");
+    }
+
+
+    void Update() //GameLogic
+    {
+        horizontal = inputJugador.ejeHorizontal;
+        vertical = inputJugador.ejeVertical;
+
+
+        VoltearSprite();
+
+        if (vertical != 0 || horizontal != 0)
+        {
+            SetXYAnimator();
+            animator.SetBool(correrHashCode, true);
+        }
+        else
+        {
+            animator.SetBool(correrHashCode, false);
+        }
+
+    }
+
+    private void VoltearSprite()
+    {
+        //Si se buguea la animacion cambiar el signo de menor a mayor del segundo
+        if (horizontal < 0 && Mathf.Abs(vertical) < Mathf.Abs(horizontal))
+        {
+            miSprite.flipX = true;
+        }
+        else if (horizontal != 0)
+        {
+            miSprite.flipX = false;
+        }
+    }
+
+    private void SetXYAnimator()
+    {
+        animator.SetFloat("X", horizontal);
+        animator.SetFloat("Y", vertical);
+    }
+
+    void FixedUpdate() //Fisica de Movimiento
+    {
+
+        Vector2 vectorVelocidad = new Vector2(horizontal, vertical) * velocidad;
+        miRigidbody2D.velocity = vectorVelocidad;
+
+    }
+}
